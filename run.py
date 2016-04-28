898ac49a6468cb12562754d8c7b2b97a2fdbef8b
calebh93 <hawkins.caleb93@gmail.com>
2016-04-28 13:44:42 -0700

    Now returns tweet info, config with env variables

--- run.py
+++ run.py
@@ -1,9 +1,37 @@
+import oauth2
+import os
+import json
 
 
-def main():
-    """Tweet tweet."""
-    print ("Caw!")
+def oauth_req(url, key, secret, http_method="GET", post_body="",
+              http_headers=None):
+    """Generate oauth tokens and such."""
+    consumer = oauth2.Consumer(key=os.environ.get("TWITSCOPE_CONSUMER_TOKEN"),
+                               secret=os.environ.get("TWITSCOPE_CONSUMER_SECRET"))
+    token = oauth2.Token(key=key, secret=secret)
+    client = oauth2.Client(consumer, token)
+    resp, content = client.request(url, method=http_method, body=post_body,
+                                   headers=http_headers)
+    return content
+
+
+def run():
+    """Run the script."""
+    results = oauth_req("https://api.twitter.com/1.1/search/tweets.json?q=%40lifeatfulcrum&result_type=recent",
+                        os.environ.get("TWITSCOPE_APP_TOKEN"),
+                        os.environ.get("TWITSCOPE_APP_SECRET"))
+
+    tweets = []
+
+    for i in json.loads(results)["statuses"]:
+        tweet = {}
+        tweet["user"] = (i["user"]["name"] + " @" + i["user"]["screen_name"])
+        tweet["text"] = i["text"]
+        tweet["time"] = i["created_at"]
+        tweets.append(tweet)
+
+    return json.dumps(tweets)
 
 
 if __name__ == "__main__":
-    main()
+    print run()
